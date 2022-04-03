ccd1acd1e59e9829d5296c769ff29d6744d050c4
obleey <miha.oblishar@gmail.com>
2022-04-03 20:44:17 +0200

    inital

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Footer, Services, Welcome, Transactions, Navbar } from "./components";
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
